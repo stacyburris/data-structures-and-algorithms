b98764fe887185610156643524b647256b658cdc
Stacy Burris <stacy1burris@gmail.com>
2021-01-24 12:18:40 -0800

    some tests pass

--- javascript/code-challenges/graph/README.md
+++ javascript/code-challenges/graph/README.md
@@ -2,6 +2,34 @@
 
 ## Challenge
 
++ Implementing your own Graph. The graph should be represented as an adjacency list
+
+## Author: Stacy Burris
+
+### Contributors:
+
++ Simon Panek & Ricardo Barcenas
+
 ## Approach & Efficiency
 
-## API
++ **Include following methods:**
++ AddNode() - BigO Time & Space = O(1)
+  + Adds a new node to the graph
+  + Takes in the value of that node
+  + Returns the added node
++ AddEdge() - BigO Time & Space = O(1)
+  + Adds a new edge between two nodes in the graph
+  + Include the ability to have a “weight”
+  + Takes in the two nodes to be connected by the edge
+    + Both nodes should already be in the Graph
++ GetNodes() - BigO Time & Space = O(n^2)
+  + Returns all of the nodes in the graph as a collection (set, list, or similar)
++ GetNeighbors() - BigO Time & Space = O(n^2)
+  + Returns a collection of edges connected to the given node
+  + Takes in a given node 
+  + Include the weight of the connection in the returned collection
++ Size() - BigO Time & Space = O(n^2)
+  + Returns the total number of nodes in the graph
+
+
+
--- javascript/code-challenges/graph/graph.test.js
+++ javascript/code-challenges/graph/graph.test.js
@@ -8,13 +8,19 @@ describe('Graphs', () => {
     let vertex = new Vertex(5);
     let graph = new Graph();
     graph.addVertex(vertex);
-    console.log(graph);
+    console.log({ graph });
     expect(graph.getNeighbors(vertex)).toEqual([]);
   });
 
   it('2. An edge can be successfully added to the graph', () => {
-    let firstVertex = new Vertex();
-    let secondVertex = new Vertex();
+    let vertex = new Vertex(5);
+    let secondVertex = new Vertex(7);
+    let graph = new Graph();
+    graph.addVertex(vertex);
+    graph.addVertex(secondVertex);
+    graph.addDirectedEdge(vertex, secondVertex, 3);
+    console.log({ vertex }, { secondVertex }, { graph });
+    expect([graph.breadthFirstSearch(vertex)].toEqual([vertex, secondVertex]));
 
   });
   it('3. A collection of all nodes can be properly retrieved from the graph', () => {
@@ -30,9 +36,14 @@ describe('Graphs', () => {
 
   });
   it('7. A graph with only one node and edge can be properly returned', () => {
-
+    let vertex = new Vertex(5);
+    let graph = new Graph();
+    graph.addVertex(vertex);
+    expect(graph.size(vertex)).toEqual(1);
   });
   it('8. An empty graph properly returns null', () => {
-
+    let graph = new Graph();
+    console.log({ graph });
+    expect(graph.size()).toEqual(0);
   });
 });
